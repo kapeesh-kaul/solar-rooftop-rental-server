9a9f1eeeba674c95759c2fea810411c77da1a13f
Kapeesh Kaul <kkapeesh1999@gmail.com>
2025-03-15 04:58:01 -0600

    update user schema

--- app/schemas/user_schema.py
+++ app/schemas/user_schema.py
@@ -4,3 +4,7 @@ class UserSchema(BaseModel):
     name: str
     email: EmailStr
     address: str
+    rate: float
+    verified: bool = False
+    moderated: bool = False
+    coordinates: list[float] = []

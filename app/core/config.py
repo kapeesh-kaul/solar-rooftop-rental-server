f9747ebb70f1ebd5cdc15371d339d41accde2733
Kapeesh Kaul <kkapeesh1999@gmail.com>
2025-03-15 17:13:27 -0600

    update config

--- app/core/config.py
+++ app/core/config.py
@@ -7,6 +7,8 @@ class Settings:
     MONGO_URI: str = os.getenv("MONGO_URI")
     MONGO_DB_NAME: str = os.getenv("MONGO_DB_NAME")
     OLLAMA_MODEL: str = os.getenv("OLLAMA_MODEL")
+    SUPABASE_URL: str = os.getenv("SUPABASE_URL")
+    SUPABASE_ANON_KEY: str = os.getenv("SUPABASE_ANON_KEY")
     
 
     def __init__(self):
@@ -16,5 +18,9 @@ class Settings:
             raise ValueError("OLLAMA_MODEL is not set in the environment variables.")
         if not self.MONGO_DB_NAME:
             raise ValueError("MONGO_DB_NAME is not set in the environment variables.")
+        if not self.SUPABASE_URL:
+            raise ValueError("SUPABASE_URL is not set in the environment variables.")
+        if not self.SUPABASE_ANON_KEY:
+            raise ValueError("SUPABASE_ANON_KEY is not set in the environment variables.")
     
 settings = Settings()

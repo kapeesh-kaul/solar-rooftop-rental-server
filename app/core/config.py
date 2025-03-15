2b8f9331a253c0b707c2e31f9b7e7e005b179ed9
Kapeesh Kaul <kkapeesh1999@gmail.com>
2025-03-15 04:57:41 -0600

    update config

--- app/core/config.py
+++ app/core/config.py
@@ -4,13 +4,17 @@ from dotenv import load_dotenv
 load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), '../../.env'))
 
 class Settings:
-    MONGO_URI: str = os.getenv("MONGODB_URI")
+    MONGO_URI: str = os.getenv("MONGO_URI")
+    MONGO_DB_NAME: str = os.getenv("MONGO_DB_NAME")
     OLLAMA_MODEL: str = os.getenv("OLLAMA_MODEL")
+    
 
     def __init__(self):
         if not self.MONGO_URI:
-            raise ValueError("MONGODB_URI is not set in the environment variables.")
+            raise ValueError("MONGO_URI is not set in the environment variables.")
         if not self.OLLAMA_MODEL:
             raise ValueError("OLLAMA_MODEL is not set in the environment variables.")
+        if not self.MONGO_DB_NAME:
+            raise ValueError("MONGO_DB_NAME is not set in the environment variables.")
     
 settings = Settings()
